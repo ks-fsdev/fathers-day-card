31da7f933ee9a58fbd43ce388c4e747c56b90743
Khushi Srivastava <sweetkhushi2000@gmail.com>
2025-06-16 14:22:43 +0530

    added music

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 btn = document.querySelector("button");
 para = document.querySelector("p");
 
+function playMusic() {
+  const audio = document.getElementById("bgMusic");
+  audio.play();
+}
+
 btn.addEventListener("click", function () {
   para.classList.toggle("hide");
+  playMusic();
 
   if (para.classList.contains("hide")) {
     btn.innerText = "CLick to See Message!";
